197148667b5b38f22403a158beea15f34211492e
Anurag Dutt <anuragdutt3668@gmail.com>
2023-02-02 09:00:43 +0530

    Uploading

---
+++ javascript.js
@@ -0,0 +1,35 @@
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+    navLinks.style.right = "0";
+}
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
+const button = document.querySelector("#mybutton");
+const section = document.querySelector("#Details");
+
+button.addEventListener("click", function () {
+
+    section.scrollIntoView({
+        behavior: "smooth"
+    });
+});
